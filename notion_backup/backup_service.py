25a667de69dbf7edbfbc4764dbde9729d9b6c6b6
Hugo Lime <ligohu@outlook.fr>
2020-05-25 23:29:51 +0200

    Export workspace and download file

--- notion_backup/backup_service.py
+++ notion_backup/backup_service.py
@@ -1,17 +1,25 @@
+import shutil
+from datetime import datetime
 from operator import itemgetter
+from pathlib import Path
+from time import sleep
 
+import requests
 from prompt_toolkit import prompt
+from tqdm import tqdm
 
 from notion_backup.configuration_service import ConfigurationService
 from notion_backup.notion_client import NotionClient
 
+STATUS_WAIT_TIME = 5
+block_size = 1024  # 1 Kibibyte
 
 class BackupService:
     def __init__(self):
         self.configuration_service = ConfigurationService()
         self.notion_client = NotionClient(self.configuration_service)
 
-    def login(self):
+    def _login(self):
         email = self.configuration_service.get_key("email")
         if email:
             email = prompt("Email address: ", default=email)
@@ -27,11 +35,21 @@ class BackupService:
         self.configuration_service.write_key("token", token)
         print("Congratulations, you have been successfully authenticated")
 
+    def _download_file(self, url, export_file):
+        with requests.get(url, stream=True, allow_redirects=True) as response:
+            total_size = int(response.headers.get('content-length', 0))
+            tqdm_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
+            with export_file.open("wb") as export_file_handle:
+                for data in response.iter_content(block_size):
+                    tqdm_bar.update(len(data))
+                    export_file_handle.write(data)
+            tqdm_bar.close()
+
     def backup(self):
         print("Login to notion if necessary")
         token = self.configuration_service.get_key("token")
         if not token:
-            self.login()
+            self._login()
 
         user_content = self.notion_client.get_user_content()
 
@@ -44,20 +62,35 @@ class BackupService:
         ]
         print("Available spaces:")
         for (space_id, space_name) in spaces:
-            print(f'\t- {space_name}: {space_id}')
-        space_id = self.configuration_service.get_key('space_id')
+            print(f"\t- {space_name}: {space_id}")
+        space_id = self.configuration_service.get_key("space_id")
         space_id = prompt("Select space id: ", default=(space_id or spaces[0][0]))
 
         if space_id not in map(itemgetter(0), spaces):
-            raise Exception('Selected space id not in list')
+            raise Exception("Selected space id not in list")
 
-        self.configuration_service.write_key('space_id', space_id)
+        self.configuration_service.write_key("space_id", space_id)
+
         print("Launching export task")
+        task_id = self.notion_client.launch_export_task(space_id)
+        print(f"Export task {task_id} has been launched")
 
+        while True:
+            task_status = self.notion_client.get_user_task_status(task_id)
+            if task_status["status"]["type"] == "complete":
+                break
+            print(
+                f"...Export still in progress, waiting for {STATUS_WAIT_TIME} seconds"
+            )
+            sleep(STATUS_WAIT_TIME)
+        print('Export task is finished')
 
+        export_link = task_status["status"]["exportURL"]
+        print(f"Downloading zip export from {export_link}")
 
-        print("Waiting for export task to complete")
-        print("Downloading zip export")
+        export_file_name = f'export_{space_id}_{datetime.now().strftime("%Y%m%d")}.zip'
+
+        self._download_file(export_link, Path(export_file_name))
 
 
 if __name__ == "__main__":
--- notion_backup/notion_client.py
+++ notion_backup/notion_client.py
@@ -22,14 +22,17 @@ class NotionClient:
         )
         response.raise_for_status()
         json_response = response.json()
-        return {"csrf_state": json_response["csrfState"], "csrf_cookie": response.cookies["csrf"]}
+        return {
+            "csrf_state": json_response["csrfState"],
+            "csrf_cookie": response.cookies["csrf"],
+        }
 
     def get_token(self, csrf_values, otp):
         response = requests.request(
             "POST",
             f"{NOTION_API_ROOT}/loginWithEmail",
             json={"state": csrf_values["csrf_state"], "password": otp},
-            cookies={'csrf': csrf_values["csrf_cookie"]},
+            cookies={"csrf": csrf_values["csrf_cookie"]},
         )
         response.raise_for_status()
         return response.cookies["token_v2"]
@@ -39,13 +42,35 @@ class NotionClient:
             "POST",
             f"{NOTION_API_ROOT}/{path}",
             json=body,
-            cookies={
-                'token_v2': self.configuration_service.get_key('token')
-            },
+            cookies={"token_v2": self.configuration_service.get_key("token")},
         )
         response.raise_for_status()
         return response.json()
 
     def get_user_content(self):
-        return self._send_post_request('loadUserContent', {})["recordMap"]
+        return self._send_post_request("loadUserContent", {})["recordMap"]
 
+    def launch_export_task(self, space_id):
+        return self._send_post_request(
+            "enqueueTask",
+            {
+                "task": {
+                    "eventName": "exportSpace",
+                    "request": {
+                        "spaceId": space_id,
+                        "exportOptions": {
+                            "exportType": "markdown",
+                            "timeZone": "Europe/Paris",
+                            "locale": "en",
+                        },
+                    },
+                }
+            },
+        )["taskId"]
+
+    def get_user_task_status(self, task_id):
+        task_statuses = self._send_post_request("getTasks", {"taskIds": [task_id]})[
+            "results"
+        ]
+
+        return list(filter(lambda task_status: task_status["id"] == task_id, task_statuses))[0]
